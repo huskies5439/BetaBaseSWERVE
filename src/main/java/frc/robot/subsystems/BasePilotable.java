cb7c26f457260f55ba7368336c40733975e15fe2
Pierre-André <royp1@csdps.qc.ca>
2022-04-15 08:59:51 -0400

    Manque juste à mieux comprendre les swerves et changer mes erreurs

--- src/main/java/frc/robot/subsystems/BasePilotable.java
+++ src/main/java/frc/robot/subsystems/BasePilotable.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.cscore.VideoEvent.Kind;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -14,8 +13,6 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class BasePilotable extends SubsystemBase {
 
   private ADXRS450_Gyro gyro = new ADXRS450_Gyro();
-  
-  private SwerveDriveOdometry odometry;
 
   public BasePilotable() {
     //Resets initiaux
@@ -26,36 +23,55 @@ public class BasePilotable extends SubsystemBase {
     //setRamp(Constants.kRampTeleOp);
     //setBrake(false);
     
-    Translation2d frontLeftLocation = new Translation2d(0, 0);// mettre les mesures depuis le centre du robot
-    Translation2d frontRightLocation = new Translation2d(0, 0);// mettre les mesures depuis le centre du robot
-    Translation2d backLeftLocation = new Translation2d(0, 0);// mettre les mesures depuis le centre du robot
-    Translation2d backRightLocation = new Translation2d(0, 0);// mettre les mesures depuis le centre du robot
+    var frontLeftState = new SwerveModuleState(0, Rotation2d.fromDegrees(0));
+    var frontRightState = new SwerveModuleState(0, Rotation2d.fromDegrees(0));
+    var backLeftState = new SwerveModuleState(0, Rotation2d.fromDegrees(0));
+    var backRightState = new SwerveModuleState(0, Rotation2d.fromDegrees(0));
 
-    SwerveDriveKinematics kinematics = new SwerveDriveKinematics(
-      frontLeftLocation, frontRightLocation, backLeftLocation, backRightLocation
+    ChassisSpeeds chassisSpeeds = kinematics.toChassisSpeeds(
+      frontLeftState, frontRightState, backLeftState, backRightState
     );
-
-    ChassisSpeeds speeds = ChassisSpeeds.fromFieldRelativeSpeeds(2.0, 2.0, Math.PI/2.0, Rotation2d.fromDegrees(45.0));
-
-    SwerveModuleState[] moduleStates = kinematics.toSwerveModuleStates(speeds);
-
-    SwerveModuleState frontLeft = moduleStates[0];
-
-    SwerveModuleState frontRight = moduleStates[1];
-
-    SwerveModuleState backLeft = moduleStates[2];
-
-    SwerveModuleState backRight = moduleStates[3];
+    
+    double forward = chassisSpeeds.vxMetersPerSecond;
+    double sideways = chassisSpeeds.vyMetersPerSecond;
+    double angular = chassisSpeeds.omegaRadiansPerSecond;
 
     //Optimisation
-    //var frontLeftOptimized = SwerveModuleState.optimize(frontLeft, new Rotation2d(/*encodeur pour angle*/.getDistance()));
-    //var frontRightOptimized = SwerveModuleState.optimize(frontRight, new Rotation2d(/*encodeur pour angle*/.getDistance()));
-    //var backLeftOptimized = SwerveModuleState.optimize(backLeft, new Rotation2d(/*encodeur pour angle*/.getDistance()));
-    //var backRightOptimized = SwerveModuleState.optimize(backRight, new Rotation2d(/*encodeur pour angle*/.getDistance()));
+    var frontLeftOptimized = SwerveModuleState.optimize(frontLeftState, new Rotation2d(/*encodeur pour angle.getDistance*/getAngle/*c pas le bon*/()));
+    var frontRightOptimized = SwerveModuleState.optimize(frontRightState, new Rotation2d(/*encodeur pour angle.getDistance*/getAngle/*c pas le bon*/()));
+    var backLeftOptimized = SwerveModuleState.optimize(backLeftState, new Rotation2d(/*encodeur pour angle.getDistance*/getAngle/*c pas le bon*/()));
+    var backRightOptimized = SwerveModuleState.optimize(backRightState, new Rotation2d(/*encodeur pour angle.getDistance*/getAngle/*c pas le bon*/()));
   }
 
+  Translation2d frontLeftLocation = new Translation2d(0, 0);// mettre les mesures depuis le centre du robot
+  Translation2d frontRightLocation = new Translation2d(0, 0);// mettre les mesures depuis le centre du robot
+  Translation2d backLeftLocation = new Translation2d(0, 0);// mettre les mesures depuis le centre du robot
+  Translation2d backRightLocation = new Translation2d(0, 0);// mettre les mesures depuis le centre du robot
+
+  SwerveDriveKinematics kinematics = new SwerveDriveKinematics(
+    frontLeftLocation, frontRightLocation, backLeftLocation, backRightLocation
+  );
+
+  SwerveDriveOdometry odometry = new SwerveDriveOdometry(kinematics, gyro.getRotation2d()/*Pas sur*/, new Pose2d(5.0, 13.5, new Rotation2d()));
+
+  ChassisSpeeds speeds = ChassisSpeeds.fromFieldRelativeSpeeds(2.0, 2.0, Math.PI/2.0, Rotation2d.fromDegrees(45.0));
+
+  SwerveModuleState[] moduleStates = kinematics.toSwerveModuleStates(speeds);
+
+  SwerveModuleState frontLeftState = moduleStates[0];
+
+  SwerveModuleState frontRightState = moduleStates[1];
+
+  SwerveModuleState backLeftState = moduleStates[2];
+
+  SwerveModuleState backRightState = moduleStates[3];
+
   @Override
-  public void periodic() {}
+  public void periodic() {
+    var gyroAngle = Rotation2d.fromDegrees(-getAngle());
+
+    Pose2d pose = odometry.update(gyroAngle, frontLeftState/*.getState*/);
+  }
 
   @Override
   public void simulationPeriodic() {
@@ -81,7 +97,7 @@ public class BasePilotable extends SubsystemBase {
 
 //////////////////////////////////////// Pose et Odométrie/////////////////////////////////////////////
 
-  /*public Pose2d getPose() {
+  public Pose2d getPose() {
     return odometry.getPoseMeters();
   }
 
@@ -100,6 +116,6 @@ public class BasePilotable extends SubsystemBase {
     //resetEncodeur();
     resetGyro();
     odometry.resetPosition(pose, Rotation2d.fromDegrees(getAngle()));
-  }*/
+  }
 
 }
